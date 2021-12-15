c36e93c342363bd60e569984a220858c27acd571
bogialmasi <boglarkaalmasi@gmail.com>
2021-12-15 11:40:47 +0100

    --

--- src/main/java/hu/kisallatokSecurity/config/SecurityConfig.java
+++ src/main/java/hu/kisallatokSecurity/config/SecurityConfig.java
@@ -4,29 +4,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Autowired
-    public void configueAuth(AuthenticationManagerBuilder auth){
-        try{
-        auth.inMemoryAuthentication()
-                .withUser("user").password("{noop}USER").roles("USER")
-                .and()
-                .withUser("admin").password("{noop}ADMIN").roles("ADMIN", "USER");}
-        catch (Exception e) {
-            System.out.println("EX in SecurityConfig.configureAuth: " + e.getMessage());
+    public void configureAuth(AuthenticationManagerBuilder auth){
+        try {
+            auth.inMemoryAuthentication()
+                    .withUser("user").password("{noop}user").roles("USER")
+                    .and()
+                    .withUser("admin").password("{noop}admin").roles("ADMIN", "USER");
+        } catch (Exception ex) {
+            System.out.println("ex in SecurityConfig.configureAuth(): "+ex.getMessage());
         }
     }
 
     @Override
-    public void configure(HttpSecurity http) throws Exception{
+    public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/pictures", "/pics/**").permitAll()
+                .antMatchers("/", "/pictures", "/pics/**", "/new-owner").permitAll()
                 .antMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
---
+++ src/main/java/hu/kisallatokSecurity/controllers/FrontController.java
@@ -0,0 +1,46 @@
+package hu.kisallatokSecurity.controllers;
+
+import hu.kisallatokSecurity.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class FrontController {
+
+    @Autowired
+    private OwnerService ownerService;
+
+    @GetMapping("/")
+    public String front(){
+        return "index";
+    }
+
+    @GetMapping("/pictures")
+    public String pics(){
+        return "pictures";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/new-owner")
+    public String newowner(){
+        return "newowner";
+    }
+
+    @PostMapping("/new-owner")
+    public String newOwner(@RequestParam("ownername") String name,
+                           @RequestParam("owneraddress") String address,
+                           Model model){
+        System.out.println("name: "+name);
+        int status = ownerService.newOwner(name, address);
+        model.addAttribute("status", status);
+        return "admin";
+    }
+}
